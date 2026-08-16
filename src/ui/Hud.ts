cc11996c0610bd7a9ee2cc2b7c486cea71dc8f65
Majveia <taomajveia@gmail.com>
2026-08-15 20:35:57 -0400

    Add Hud class for UI management

---
+++ src/ui/Hud.ts
@@ -0,0 +1,7 @@
+export class Hud {
+  element = document.createElement('div');
+  constructor(_engine: any) {}
+  setVeil(_v: number) {}
+  setContext(_c: string) {}
+  setVisible(_v: boolean) {}
+}
